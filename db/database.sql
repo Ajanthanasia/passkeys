fd11066285031d07dfe0365cbd149d56fb5afa64
Ajanthan <ajanthanasia@gmail.com>
2024-07-07 12:14:17 +0530

    done

---
+++ db/database.sql
@@ -0,0 +1,32 @@
+create table
+    users (
+        id int (16) not null auto_increment,
+        name varchar(255) null default null,
+        email varchar(255) null default null,
+        email_verified_at timestamp default null,
+        password varchar(255) null default null,
+        status varchar(16) null default null,
+        created_at timestamp null default null,
+        updated_at timestamp null default null,
+        primary key (id)
+    );
+
+create table
+    face_recognitions (
+        id int (16) not null auto_increment,
+        user_id int (16) not null,
+        img varchar(255) null default null,
+        status varchar(16) null default null,
+        created_at timestamp null default null,
+        updated_at timestamp null default null,
+    );
+
+create table
+    finger_prints (
+        id int (16) not null auto_increment,
+        user_id int (16) not null,
+        finger varchar(255) null default null,
+        status varchar(16) null default null,
+        created_at timestamp null default null,
+        updated_at timestamp null default null,
+    );
