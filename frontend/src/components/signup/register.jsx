4704ee7388a40b7fbd67d7522b6b91d2e31c0552
Ajanthan <ajanthanasia@gmail.com>
2024-07-13 17:23:44 +0530

    done

--- frontend/src/components/signup/register.jsx
+++ frontend/src/components/signup/register.jsx
@@ -1,7 +1,58 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 function RegisterPage() {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const [usernameError, setUsernameError] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [confirmPasswordError, setConfirmPasswordError] = useState("");
+
+    const signupUrl = "#";
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            setUsernameError("");
+            setEmailError("");
+            setPasswordError("");
+            setConfirmPasswordError("");
+            if (username === "") {
+                setUsernameError("This field is required");
+            }
+            if (email === "") {
+                setEmailError("This field is required");
+            }
+            if (password === "") {
+                setPasswordError("This field is required");
+            }
+            if (confirmPassword === "") {
+                setConfirmPasswordError("This field is required");
+            }
+            if (username && email && password && confirmPassword) {
+                if (password !== confirmPassword) {
+                    setConfirmPasswordError("Passwords does not matched");
+                } else {
+                    const res = await axios.post(`${signupUrl}`, {
+                        username: username,
+                        email: email,
+                        password: password,
+                        confirm_password: confirmPassword,
+                    });
+                    console.log(res);
+                    console.log('succ');
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
         <div className="panel panel-default">
@@ -10,45 +61,67 @@ function RegisterPage() {
                     <span className="btn btn-default form-control">Register your account</span>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-md-8">
-                    <b><label htmlFor="">Name</label></b>
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Enter username"
-                    />
+            <form onSubmit={handleSubmit}>
+                <div className="row">
+                    <div className="col-md-8">
+                        <b><label htmlFor="">Name</label></b>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter your username..."
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <code>{usernameError}</code>
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-md-8">
-                    <b><label htmlFor="">Email</label></b>
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Enter email"
-                    />
+                <div className="row">
+                    <div className="col-md-8">
+                        <b><label htmlFor="">Email</label></b>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter your email..."
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <code>{emailError}</code>
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-md-8">
-                    <b><label htmlFor="">Password</label></b>
-                    <input type="text" className="form-control" />
+                <div className="row">
+                    <div className="col-md-8">
+                        <b><label htmlFor="">Password</label></b>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter your password..."
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <code>{passwordError}</code>
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-md-8">
-                    <b><label htmlFor="">Confirm Password</label></b>
-                    <input type="text" className="form-control" />
+                <div className="row">
+                    <div className="col-md-8">
+                        <b><label htmlFor="">Confirm Password</label></b>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Confirm your password..."
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                        <code>{confirmPasswordError}</code>
+                    </div>
                 </div>
-            </div>
-            <div className="row mt-1">
-                <div className="col-md-8">
-                    <button className="btn btn-primary form-control">
-                        Sign Up
-                    </button>
+                <div className="row mt-1">
+                    <div className="col-md-8">
+                        <button type="submit" className="btn btn-primary form-control">
+                            Sign Up
+                        </button>
+                    </div>
                 </div>
-            </div>
+            </form>
             <div className="row mt-1">
                 <div className="col-md-8">
                     <Link to="/">
